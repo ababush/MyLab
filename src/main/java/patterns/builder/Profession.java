0e719e6ce410df0620e88ca5930e58ed46f8be14
ababush <43743603+ababush@users.noreply.github.com>
2018-10-31 16:04:57 +0200

    Add files via upload

---
+++ src/main/java/patterns/builder/Profession.java
@@ -0,0 +1,13 @@
+package patterns.builder;
+/**
+ * Profession enumeration
+ */
+public enum Profession {
+
+    WARRIOR, THIEF, MAGE, PRIEST;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+}
